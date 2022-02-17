9454d9d191c21a25000aa00f1c934f5781ed50d4
Arvin Singh Kushwaha <arvin.singh.kushwaha@gmail.com>
2022-02-17 10:50:17 -0500

    Updated README.md and Complete Soft-Sphere

--- README.md
+++ README.md
@@ -9,6 +9,5 @@ Simulations) computer assignments. Below, you can find a description of each pro
 
 This project was about tackling the Lennard-Jones potential with a cutoff to
 ignore the attractive forces between the atoms in 2 dimensions. For this
-project, I simply assumed that our atoms were Argon and implemented periodic
-boundary conditions (using minimum image convention).
+project I implemented periodic boundary conditions (using minimum image convention).
 
--- soft-sphere/main.cpp
+++ soft-sphere/main.cpp
@@ -1,8 +1,10 @@
-#include <string>
 #include <vector>
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <random>
+#include <iomanip>
+
 using size_t = std::size_t;
 
 
@@ -35,116 +37,141 @@ struct Vec2 {
     Vec2 operator<=(const Vec2& v) const;
     Vec2 operator>=(const Vec2& v) const;
 
+    double norm() const;
     double norm2() const;
     double dot(const Vec2& v) const;
     double cross(const Vec2& v) const;
+
+    static Vec2 unitRand();
 };
 
-inline double& Vec2::operator[] (size_t i) { return *(&x + i); }
-inline double Vec2::operator[] (size_t i) const { return *(&x + i); }
+Vec2 operator+(double a, const Vec2& v);
+Vec2 operator-(double a, const Vec2& v);
+Vec2 operator*(double a, const Vec2& v);
+Vec2 operator/(double a, const Vec2& v);
 
-inline Vec2 Vec2::operator+(const Vec2& v) const { return {x + v.x, y + v.y}; }
-inline Vec2 Vec2::operator-(const Vec2& v) const { return {x - v.x, y - v.y}; }
-inline Vec2 Vec2::operator*(const Vec2& v) const { return {x * v.x, y * v.y}; }
-inline Vec2 Vec2::operator/(const Vec2& v) const { return {x / v.x, y / v.y}; }
+std::ostream& operator<<(std::ostream& os, const Vec2& v);
 
-inline Vec2 Vec2::operator+(double a) const { return {x + a, y + a}; }
-inline Vec2 Vec2::operator-(double a) const { return {x - a, y - a}; }
-inline Vec2 Vec2::operator*(double a) const { return {x * a, y * a}; }
-inline Vec2 Vec2::operator/(double a) const { return {x / a, y / a}; }
-inline Vec2 Vec2::operator-() const { return {-x, -y}; }
-
-inline Vec2& Vec2::operator+=(const Vec2& v) { x += v.x; y += v.y; return *this; }
-inline Vec2& Vec2::operator-=(const Vec2& v) { x -= v.x; y -= v.y; return *this; }
-inline Vec2& Vec2::operator*=(double a) { x *= a; y *= a; return *this; }
-inline Vec2& Vec2::operator/=(double a) { x /= a; y /= a; return *this; }
-
-inline Vec2 Vec2::operator==(const Vec2& v) const { return {double(x == v.x), double(y == v.y)}; }
-inline Vec2 Vec2::operator!=(const Vec2& v) const { return {double(x != v.x), double(y != v.y)}; }
-inline Vec2 Vec2::operator<(const Vec2& v) const { return {double(x < v.x), double(y < v.y)}; }
-inline Vec2 Vec2::operator>(const Vec2& v) const { return {double(x > v.x), double(y > v.y)}; }
-inline Vec2 Vec2::operator<=(const Vec2& v) const { return {double(x <= v.x), double(y <= v.y)}; }
-inline Vec2 Vec2::operator>=(const Vec2& v) const { return {double(x >= v.x), double(y >= v.y)}; }
-
-inline double Vec2::norm2() const { return x * x + y * y; }
-inline double Vec2::dot(const Vec2& v) const { return x * v.x + y * v.y; }
-inline double Vec2::cross(const Vec2& v) const { return x * v.y - y * v.x; }
-
-inline Vec2 operator+(double a, const Vec2& v) { return v + a; }
-inline Vec2 operator-(double a, const Vec2& v) { return -v + a; }
-inline Vec2 operator*(double a, const Vec2& v) { return v * a; }
-inline Vec2 operator/(double a, const Vec2& v) { return { a / v.x, a / v.y}; }
-
-struct Energy {
+struct Macrostates {
     double kinetic, potential;
+    double pressure, temperature;
 };
 
-class HardBallSimulation {
+class SoftSphereSimulation {
     public:
         double time = 0;
-        double mass, sigma, epsilon, rc2, dt;
+        double mass, sigma, epsilon, rc2, dt, velMag;
         Vec2 boxSize;
-        Energy energy;
-        Vec2 totalForce;
-        Vec2 totalMomentum;
+        Macrostates macrostate;
+        double furthestDistance;
+        Vec2 totalForce, totalMomentum, centerOfMass, centerOfMassVelocity;
         std::vector<Vec2> pos;
         std::vector<Vec2> vel;
         std::vector<Vec2> force;
 
-        HardBallSimulation(
-            double _mass, 
+        SoftSphereSimulation(
+            double _mass,
             double _sigma,
-            double _epsilon, 
+            double _epsilon,
             Vec2 _boxSize,
-            double _dt
-        ) : mass(_mass), 
-            sigma(_sigma), 
-            epsilon(_epsilon), 
-            rc2(_sigma * _sigma * powf(2., 1./3.)), 
+            double _dt,
+            double _velMag
+        ) : mass(_mass),
+            sigma(_sigma),
+            epsilon(_epsilon),
+            rc2(_sigma * _sigma * std::pow(2., 1./3.)),
             dt(_dt),
+            velMag(_velMag),
             boxSize(_boxSize) {}
 
-        void initialize_particles(size_t n_particles);
         void initialize_particles_grid(size_t n_x, size_t n_y);
         void calculate_force();
         void update();
 };
 
-std::ostream& operator<<(std::ostream&, const HardBallSimulation&);
+std::ostream& operator<<(std::ostream&, const SoftSphereSimulation&);
 
-void HardBallSimulation::initialize_particles(size_t n_particles) {
-    pos.resize(n_particles); vel.resize(n_particles); force.resize(n_particles);
-    for (size_t i = 0; i < n_particles; i++) {
-        pos[i] = Vec2({
-                    (2.0 * rand() / double(RAND_MAX) - 1.0),
-                    (2.0 * rand() / double(RAND_MAX) - 1.0)
-                }) * boxSize;
-        
-        vel[i].x = vel[i].y = 0;
-        force[i].x = force[i].y = 0;
-    }
+double& Vec2::operator[] (size_t i) { return *(&x + i); }
+double Vec2::operator[] (size_t i) const { return *(&x + i); }
+
+Vec2 Vec2::operator+(const Vec2& v) const { return {x + v.x, y + v.y}; }
+Vec2 Vec2::operator-(const Vec2& v) const { return {x - v.x, y - v.y}; }
+Vec2 Vec2::operator*(const Vec2& v) const { return {x * v.x, y * v.y}; }
+Vec2 Vec2::operator/(const Vec2& v) const { return {x / v.x, y / v.y}; }
+
+Vec2 Vec2::operator+(double a) const { return {x + a, y + a}; }
+Vec2 Vec2::operator-(double a) const { return {x - a, y - a}; }
+Vec2 Vec2::operator*(double a) const { return {x * a, y * a}; }
+Vec2 Vec2::operator/(double a) const { return {x / a, y / a}; }
+Vec2 Vec2::operator-() const { return {-x, -y}; }
+
+Vec2& Vec2::operator+=(const Vec2& v) { x += v.x; y += v.y; return *this; }
+Vec2& Vec2::operator-=(const Vec2& v) { x -= v.x; y -= v.y; return *this; }
+Vec2& Vec2::operator*=(double a) { x *= a; y *= a; return *this; }
+Vec2& Vec2::operator/=(double a) { x /= a; y /= a; return *this; }
+
+Vec2 Vec2::operator==(const Vec2& v) const { return {double(x == v.x), double(y == v.y)}; }
+Vec2 Vec2::operator!=(const Vec2& v) const { return {double(x != v.x), double(y != v.y)}; }
+Vec2 Vec2::operator<(const Vec2& v) const { return {double(x < v.x), double(y < v.y)}; }
+Vec2 Vec2::operator>(const Vec2& v) const { return {double(x > v.x), double(y > v.y)}; }
+Vec2 Vec2::operator<=(const Vec2& v) const { return {double(x <= v.x), double(y <= v.y)}; }
+Vec2 Vec2::operator>=(const Vec2& v) const { return {double(x >= v.x), double(y >= v.y)}; }
+
+double Vec2::norm() const { return std::sqrt(norm2()); }
+double Vec2::norm2() const { return x * x + y * y; }
+double Vec2::dot(const Vec2& v) const { return x * v.x + y * v.y; }
+double Vec2::cross(const Vec2& v) const { return x * v.y - y * v.x; }
+
+Vec2 Vec2::unitRand() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::normal_distribution<double> d(0., 1.);
+    Vec2 rand;
+    do {
+        rand = {d(gen), d(gen)};
+    } while (rand.norm2() == 0.);
+    return rand / rand.norm();
 }
 
-void HardBallSimulation::initialize_particles_grid(size_t n_x, size_t n_y) {
-    size_t n_particles = n_x * n_y;
-    pos.resize(n_particles); vel.resize(n_particles); force.resize(n_particles);
+Vec2 operator+(double a, const Vec2& v) { return v + a; }
+Vec2 operator-(double a, const Vec2& v) { return -v + a; }
+Vec2 operator*(double a, const Vec2& v) { return v * a; }
+Vec2 operator/(double a, const Vec2& v) { return { a / v.x, a / v.y}; }
 
+std::ostream& operator<<(std::ostream& os, const Vec2& v) {
+    os << "(" << v.x << ", " << v.y << ")";
+    return os;
+}
+
+void SoftSphereSimulation::initialize_particles_grid(size_t n_x, size_t n_y) {
+    size_t n_particles = n_x * n_y;
+    pos.resize(n_particles);
+    vel.resize(n_particles);
+    force.resize(n_particles);
+
+    Vec2 nVec = {double(n_x), double(n_y)};
+    Vec2 COMVelocity = {0., 0.};
     for (size_t x = 0; x < n_x; x++) {
         for (size_t y = 0; y < n_y; y++) {
-            size_t i = x + (n_x * y);
-            pos[i] = Vec2({
-                        (2.0 * x / double(n_x) - 1.0),
-                        (2.0 * y / double(n_y) - 1.0)
-                    }) * boxSize;
+            Vec2 coord = Vec2({double(x), double(y)});
 
-            vel[i].x = vel[i].y = 0;
+            size_t i = x + (n_x * y);
+            pos[i] = ((coord + 0.5) / nVec - 0.5) * boxSize;
+
+            vel[i] = velMag * Vec2::unitRand();
+            COMVelocity += vel[i] * mass;
+
             force[i].x = force[i].y = 0;
         }
     }
+    COMVelocity /= mass * n_particles;
+    for (size_t i = 0; i < n_particles; i++) {
+        vel[i] -= COMVelocity;
+    }
 }
 
-void HardBallSimulation::calculate_force() {
-    energy.potential = 0;
+void SoftSphereSimulation::calculate_force() {
+    macrostate.potential = 0;
     totalForce.x = totalForce.y = 0;
 
     for (size_t i = 0; i < force.size(); i++)
@@ -165,7 +192,7 @@ void HardBallSimulation::calculate_force() {
             force[i] += temp_force; // add to i
             force[j] -= temp_force; // subtract from j
 
-            energy.potential += 4 * epsilon * (powf(sigma2 / dist2, 6) - powf(sigma2 / dist2, 3));
+            macrostate.potential += 4 * epsilon * (powf(sigma2 / dist2, 6) - powf(sigma2 / dist2, 3)) + epsilon;
         }
     }
 
@@ -173,80 +200,160 @@ void HardBallSimulation::calculate_force() {
         totalForce += force[i];
 }
 
-void HardBallSimulation::update() {
+void SoftSphereSimulation::update() {
     calculate_force();
+    centerOfMass.x = centerOfMass.y = 0;
+    furthestDistance = 0;
     // Kick-Drift-Kick scheme (Leapfrog)
     for (size_t i = 0; i < pos.size(); ++i) {
         vel[i] += force[i] * dt / (2. * mass);
         pos[i] += vel[i] * dt;
+        centerOfMass += mass * pos[i];
+        furthestDistance = std::max(furthestDistance, pos[i].norm2());
     }
+
+    centerOfMass /= mass * pos.size();
     time += dt / 2.0;
     calculate_force();
 
-    energy.kinetic = 0;
+    macrostate.kinetic = 0;
     totalMomentum.x = totalMomentum.y = 0;
     for (size_t i = 0; i < pos.size(); ++i) {
         vel[i] += force[i] * dt / (2. * mass);
-        energy.kinetic += vel[i].norm2() * mass / 2.0;
+        macrostate.kinetic += vel[i].norm2() * mass / 2.0;
         totalMomentum += vel[i] * mass;
     }
+    centerOfMassVelocity = totalMomentum / (mass * pos.size());
+
+    for (size_t i = 0; i < pos.size(); ++i) {
+        vel[i] -= centerOfMassVelocity;
+        pos[i] -= centerOfMass;
+    }
+
+    double avg_kinetic = macrostate.kinetic / pos.size();
+    const double boltzmann = 0.83144; // Boltzmann constant in (amu * A^2 / ps^2)/K
+    double posForceDotProduct = 0;
+    for (size_t i = 0; i < pos.size(); ++i) {
+        posForceDotProduct += force[i].dot(pos[i]);
+    }
+
+    macrostate.temperature = 2. * avg_kinetic / (2. * boltzmann);
+    macrostate.pressure = (macrostate.temperature * pos.size() + posForceDotProduct / 2.) / (boxSize.x * boxSize.y);
+    furthestDistance = std::sqrt(furthestDistance);
+
     time += dt / 2.0;
 }
 
-std::ostream& operator<<(std::ostream& out, const HardBallSimulation& sim) {
-    out << "Timestep: " << sim.time << "\n";
-    for (size_t i = 0; i < sim.pos.size(); i++) {
-        out << "\t";
-        out << sim.pos[i].x << " " << sim.pos[i].y << " ";
-        out << sim.vel[i].x << " " << sim.vel[i].y << " ";
-        out << sim.force[i].x << " " << sim.force[i].y << "\n"; 
-    }
+std::ostream& operator<<(std::ostream& out, const SoftSphereSimulation& sim) {
+    // Time | Total Energy | Total Kinetic Energy | Total Potential Energy | Pressure | Temperature | COM (v) | COM Velocity (v) | Total Force (v) | Total Momentum (v)
+    out << sim.time << "\t" 
+        << (sim.macrostate.kinetic + sim.macrostate.potential) << "\t"
+        << sim.macrostate.kinetic << "\t"
+        << sim.macrostate.potential << "\t"
+        << sim.macrostate.pressure * 1.6605e+7 << "\t"
+        << sim.macrostate.temperature << "\t"
+        << sim.centerOfMass << "\t"
+        << sim.centerOfMassVelocity << "\t"
+        << sim.totalForce << "\t"
+        << sim.totalMomentum << "\n";
     return out;
 }
 
 /**
  * We'll do our simulation with units angstrom, amu, and picoseconds
- * For epsilon = 0.01034 eV and sigma = 0.34 nm for argon (Ar), we convert to:
- * m_argon = 39.948 amu
- * sigma = 0.34 nm = 3.4 A
- * epsilon = 0.01034 eV = 99.765 amu * A^2 / ps^2
- * dt = 0.001 ps = 1 fs
  * Simulation Units:
  * Time: ps
- * Length: A
+ * Length: A = 0.1 nm
  * Mass: amu
- * Energy: amu * A^2 / ps^2 = 1.0364e-4 eV
+ * Macrostates: amu * A^2 / ps^2 = 1.0364e-4 eV
  * Force: amu * A / ps^2 = 1.0364e-4 eV / A
  * Momentum: amu * A / ps
+ * Pressure: amu / (A ps^2) = 1.0364e-4 eV / A^3 = 1.6605e+7 Pa
+ * Temperature: K = Energy / k = Energy * 1.20272 K / (amu * A^2 / ps^2)
+ * Boltzmann Constant: k = 0.83144 (amu * A^2 / ps^2) / K
  */
 int main(int argc, char *argv[]) {
-    HardBallSimulation sim(39.948, 3.4, 99.765, {100.0, 100.0}, 1e-3);
-    sim.initialize_particles_grid(80, 80); // 80^2 = 6400 particles
+    size_t n_x, n_y;
+    double dt, density, velMag, epsilon, sigma, mass, tMax = INFINITY;
 
-    std::ostream *output = argc < 2 ? &std::cout : new std::ofstream(argv[1]);
-    std::ostream &out = *output;
-    double tMax = INFINITY;
-    if (argc > 2) {
-        tMax = std::atof(argv[2]);
+    std::ostream *output;
+    switch (argc) {
+        case 2:
+            output = new std::ofstream(argv[1]);
+            break;
+        case 1:
+            output = &std::cout;
+            break;
+        default:
+            std::cerr << "\033[1;31mInvalid number of arguments.\033[0m\n";
+            return 1;
     }
+    std::ostream &out = *output;
 
-    out << sim;
+
+    // Input parameters
+    std::cout << "Enter number of particles in x direction: ";
+    std::cin >> n_x;
+    std::cout << "Enter number of particles in y direction: ";
+    std::cin >> n_y;
+    std::cout << "Enter timestep size (ps): ";
+    std::cin >> dt;
+    std::cout << "Enter density (particles per angstrom^2): ";
+    std::cin >> density;
+    std::cout << "Enter initial velocity magnitude (A/ps): ";
+    std::cin >> velMag;
+    std::cout << "Enter epsilon (eV): ";
+    std::cin >> epsilon;
+    std::cout << "Enter sigma (nm): ";
+    std::cin >> sigma;
+    std::cout << "Enter mass (amu): ";
+    std::cin >> mass;
+    std::cout << "Enter tMax (ps): ";
+    std::cin >> tMax;
+
+    Vec2 boxSize = {
+        n_x / std::sqrt(density),
+        n_y / std::sqrt(density)
+    };
+    epsilon = epsilon / 1.0364e-4; // convert eV to amu * A^2 / ps^2
+    sigma = sigma / 0.1; // convert nm to A
+
+    SoftSphereSimulation sim(mass, sigma, epsilon, boxSize, dt, velMag);
+    sim.initialize_particles_grid(n_x, n_y);
+
+    out << "Time\tTotal Energy\tTotal Kinetic Energy\tTotal Potential Energy\tPressure\tTemperature\tCOM (v)\tCOM Velocity (v)\tTotal Force (v)\tTotal Momentum (v)\n";
     while (sim.time < tMax) {
-        sim.update();
-        out << sim;
+        for (int i = 0; i < 100 && sim.time < tMax; i++) {
+            sim.update();
+            out << sim;
+        }
         printf(
             "Timestep %f:\n"
             "\tTotal Energy: %e eV (Kinetic: %e eV, Potential: %e eV)\n"
             "\tTotal Momentum: [ %e, %e ] amu A / ps\n"
-            "\tTotal Force: [ %e, %e ] amu A / ps^2\n",
+            "\tTotal Force: [ %e, %e ] amu A / ps^2\n"
+            "\tCenter of Mass: [ %e, %e ] A\n"
+            "\tCenter of Mass Velocity: [ %e, %e ] A / ps\n"
+            "\tPressure: %e Pa\n"
+            "\tTemperature: %e K\n"
+            "\tFurthest Particle: %e A\n",
             sim.time,
-            (sim.energy.kinetic + sim.energy.potential) * 1.0364e-4,
-            sim.energy.kinetic * 1.0364e-4,
-            sim.energy.potential * 1.0364e-4,
-            sim.totalMomentum.x, 
-            sim.totalMomentum.y, 
+            (sim.macrostate.kinetic + sim.macrostate.potential) * 1.0364e-4,
+            sim.macrostate.kinetic * 1.0364e-4,
+            sim.macrostate.potential * 1.0364e-4,
+            sim.totalMomentum.x,
+            sim.totalMomentum.y,
             sim.totalForce.x,
-            sim.totalForce.y
+            sim.totalForce.y,
+            sim.centerOfMass.x,
+            sim.centerOfMass.y,
+            sim.centerOfMassVelocity.x,
+            sim.centerOfMassVelocity.y,
+            sim.macrostate.pressure * 1.6605e+7,
+            sim.macrostate.temperature,
+            sim.furthestDistance
         );
     }
+    out << std::endl;
 }
+
